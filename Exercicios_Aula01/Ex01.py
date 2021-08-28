c8a26701ad03fdfdc66c00db2b032797fbe2e83b
Danielly Viana <89473256+DanyViana@users.noreply.github.com>
2021-08-28 08:47:56 -0300

    exercício 1
    resolvendo o exercício 1

---
+++ Exercicios_Aula01/Ex01.py
@@ -0,0 +1,9 @@
+# Escrever um programa em python em que tupla recebe conteúdo de outras duas e em seguida, imprime-se o conteúdo da nova tupla e o seu quarto elemento. 
+
+# Tupla são representadas por () com os elementos separados por vírgula. Uma tupla não pode ser alterada - desvantagem com relação as listas. 
+
+tupla1 = (1,2,3)
+tupla2 = (4,5,6,7)
+
+tupla1e2 = tupla1 + tupla2
+print(tupla1e2)
