853fcc74e147248a976dd45517d4dcb4dc9f6ebc
Danielly Viana <89473256+DanyViana@users.noreply.github.com>
2021-08-28 19:02:10 -0300

    Create Ex04.py

---
+++ Exercicios_Aula01/Ex04.py
@@ -0,0 +1,13 @@
+# Escreva um programa em Python que informe se um determinado número é par ou ímpar.
+
+# Definindo uma variável x inteira. Usando input para o usuário digitar o valor desejado. 
+x = int(input("Digite um número: "))
+
+if (x % 2) == 0:
+    print("É par")
+else: 
+    print("É ímpar")
+
+# x % 2 significa o resto da divisão de x por 2. Todo número par ao ser dividido por 2 tem resto zero. 
+
+# OBS: NÃO ESQUEÇA DOS DOIS PONTOS NO IF E ELSE!!! Eu esqueci...
